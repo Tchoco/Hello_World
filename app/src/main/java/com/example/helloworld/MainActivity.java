97b388689cb532ab840515ea152ff88165040305
Jerrenn <j.ren948@gmail.com>
2022-10-19 16:42:16 +0200

    commentaire de commit

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//commentaire hihi
 public class MainActivity extends AppCompatActivity {
 
     @Override
